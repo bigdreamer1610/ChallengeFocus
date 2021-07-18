b9eecb0400e6af86fd6fec3a188ca64af2b94a94
tasszz2k <tasszz2k@gmail.com>
2021-07-18 16:41:28 +0700

    fix(authen): FirebaseUtil.mGoogleSignInClient is null

--- app/src/main/java/fpt/provipluxurylimited/challengefocus/splash/SplashActivity.java
+++ app/src/main/java/fpt/provipluxurylimited/challengefocus/splash/SplashActivity.java
@@ -10,10 +10,15 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
+
 import fpt.provipluxurylimited.challengefocus.MainActivity;
 import fpt.provipluxurylimited.challengefocus.R;
 import fpt.provipluxurylimited.challengefocus.authen.AuthenticationActivity;
 import fpt.provipluxurylimited.challengefocus.helpers.Constants;
+import fpt.provipluxurylimited.challengefocus.helpers.FirebaseUtil;
 import fpt.provipluxurylimited.challengefocus.helpers.SaveSharedPreference;
 
 public class SplashActivity extends AppCompatActivity {
@@ -36,9 +41,22 @@ public class SplashActivity extends AppCompatActivity {
         });
 
         if (SaveSharedPreference.getUserId(this) != null) {
+            configureGoogleSignIn();
             Log.e("Userid", SaveSharedPreference.getUserId(this));
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
         }
+    }
+
+    private void configureGoogleSignIn() {
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        FirebaseUtil.mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        FirebaseUtil.mFirebaseAuth = FirebaseAuth.getInstance();
     }
 }
