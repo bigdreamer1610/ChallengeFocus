2885c95a27159e10d310fa25ebf0d8c359ccda82
Ngọc Lan <junebride09.02@gmail.com>
2021-07-19 09:14:24 +0700

    (update) enable sound files

--- app/src/main/java/fpt/provipluxurylimited/challengefocus/pomodoro/SoundPomoActivity.java
+++ app/src/main/java/fpt/provipluxurylimited/challengefocus/pomodoro/SoundPomoActivity.java
@@ -3,7 +3,9 @@ package fpt.provipluxurylimited.challengefocus.pomodoro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -14,6 +16,7 @@ public class SoundPomoActivity extends AppCompatActivity {
 
   ImageView btnBack;
   Button btnConfirm;
+  MediaPlayer mp;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +32,7 @@ public class SoundPomoActivity extends AppCompatActivity {
     confirm();
   }
 
-  void clickBack() {
+  private void clickBack() {
     btnBack.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View view) {
@@ -38,7 +41,7 @@ public class SoundPomoActivity extends AppCompatActivity {
     });
   }
 
-  void confirm(){
+  private void confirm(){
     btnConfirm = findViewById(R.id.btnConfirm);
     btnConfirm.setOnClickListener(new View.OnClickListener() {
       @Override
@@ -47,5 +50,86 @@ public class SoundPomoActivity extends AppCompatActivity {
         startActivity(focusPomoIntent);
       }
     });
+  }
+
+  public void playSoundDemo(View view){
+    int clickedPosition = view.getId();
+    switch (clickedPosition) {
+      case 2131296892:
+        if (mp == null) {
+        } else {
+          if(mp.isPlaying()){
+            mp.stop();
+            mp.release();
+          }
+        }
+        mp = MediaPlayer.create(this, R.raw.classroom);
+        mp.setLooping(true);
+        mp.start();
+        break;
+      case 2131296893:
+        if (mp == null) {
+        } else {
+          if(mp.isPlaying()){
+            mp.stop();
+            mp.release();
+          }
+        }
+        mp = MediaPlayer.create(this, R.raw.clock);
+        mp.setLooping(true);
+        mp.start();
+        break;
+      case 2131296894:
+        if (mp == null) {
+        } else {
+          if(mp.isPlaying()){
+            mp.stop();
+            mp.release();
+          }
+        }
+        mp = MediaPlayer.create(this, R.raw.lake);
+        mp.setLooping(true);
+        mp.start();
+        break;
+      case 2131296895:
+        if (mp == null) {
+        } else {
+          if(mp.isPlaying()){
+            mp.stop();
+            mp.release();
+          }
+        }
+        mp = MediaPlayer.create(this, R.raw.ocean);
+        mp.setLooping(true);
+        mp.start();
+        break;
+      case 2131296896:
+        if (mp == null) {
+        } else {
+          if(mp.isPlaying()){
+            mp.stop();
+            mp.release();
+          }
+        }
+        mp = MediaPlayer.create(this, R.raw.rain);
+        mp.setLooping(true);
+        mp.start();
+        break;
+      case 2131296897:
+        if (mp == null) {
+        } else {
+          if(mp.isPlaying()){
+            mp.stop();
+            mp.release();
+          }
+        }
+        mp = MediaPlayer.create(this, R.raw.rain2);
+        mp.setLooping(true);
+        mp.start();
+        break;
+      default:
+        mp.stop();
+        break;
+    }
   }
 }
